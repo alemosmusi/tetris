dad7326168e65094f7ea74c271dc0e4fbd335008
alemosmusi <alemosmusi@gmail.com>
2023-04-10 13:12:57 -0300

    Updates

---
+++ precache-manifest.d5d375cd94482315e533010bad04ab09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98667d384a9f80d7e9b06707c5d6404",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "07de01675c242894fd46",
+    "url": "/tetris/static/css/main.cb2b127c.chunk.css"
+  },
+  {
+    "revision": "32d828d886fc6c0ae1ec",
+    "url": "/tetris/static/js/2.8324371a.chunk.js"
+  },
+  {
+    "revision": "65f61a1b574cbfdae1aa1e0f7a13f7ca",
+    "url": "/tetris/static/js/2.8324371a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07de01675c242894fd46",
+    "url": "/tetris/static/js/main.c669e10e.chunk.js"
+  },
+  {
+    "revision": "1e36367202fb30be14de",
+    "url": "/tetris/static/js/runtime-main.116712b2.js"
+  },
+  {
+    "revision": "6255a470f96dd4c751f85ff8c63b0172",
+    "url": "/tetris/static/media/background.6255a470.jpg"
+  }
+]);
